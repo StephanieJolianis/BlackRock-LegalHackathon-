95a1ee5e59956008694b6ab20a2316c1cb0f1183
StephanieJolianis <stephaniejolianis@gmail.com>
2020-11-24 16:34:28 -0500

    Creacion componentes vistas y configuracion react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import Login from "./Components/Login";
+import Main from "./Components/Main";
+import DetalleAlerta from "./Components/DetalleAlerta";
+import DetalleMes from "./Components/DetalleMes";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; 
 
 function App() {
   return (
-    <h1>HelloWorld</h1>
+    <Router>
+      <Switch> 
+      <Route path="/" exact component={Login} /> 
+      <Route path="/main" exact component={Main} /> 
+      <Route path="/detallealerta" exact component={DetalleAlerta} /> 
+      <Route path="/detallemes" exact component={DetalleMes} /> 
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+ 
